bcf38aae72fb3defb6005f42fe8834de0a3314f3
ASauvage <a.sauvagepotel@gmail.com>
2024-07-05 00:05:54 +0200

    [features] add command argument to use debug mode

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from argparse import ArgumentParser
 from api_dashboard import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    ap = ArgumentParser(prog='ApiTester Dashboard',
+                        description='A dashboard to to analyze the results from ApiTester.',
+                        epilog='Developed with Flask')
+    ap.add_argument('-D', '--debug', action='store_true', dest='debug', help='Use debug mode (default: False')
+    args = ap.parse_args()
+
+    app.run(debug=args.debug)
